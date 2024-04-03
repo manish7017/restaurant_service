c57d5e27b6e08b8f891b6178fd3abac939d98f66
Manish Kumar <manish84398@gmail.com>
2024-04-04 00:18:17 +0530

    add bulk product route

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const { connect, connection, Schema, model, Types } = mongoose;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,40 +13,52 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect("mongodb://localhost:27017/restaurant_db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
+connect("mongodb://localhost:27017/restaurant_db");
+
+const db = connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
   console.log("Connected to MongoDB");
 });
 
 // product model
-const productSchema = new mongoose.Schema({
+const productSchema = new Schema({
   name: String,
   price: Number,
   description: String,
 });
-const Product = mongoose.model("Product", productSchema);
-//order model
-const orderSchema = new mongoose.Schema({
-  products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+const Product = model("Product", productSchema);
+
+// order model
+const orderSchema = new Schema({
+  products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
   totalPrice: Number,
   customerName: String,
   customerEmail: String,
   createdAt: { type: Date, default: Date.now },
 });
-const Order = mongoose.model("Order", orderSchema);
+const Order = model("Order", orderSchema);
 
 // new product
 app.post("/products", async (req, res) => {
   try {
     const { name, price, description } = req.body;
     const newProduct = new Product({ name, price, description });
+    console.log(newProduct); //debug
     await newProduct.save();
     res.status(201).json(newProduct);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// add bulk products
+app.post("/products/bulk", async (req, res) => {
+  try {
+    const products = req.body;
+    const newProducts = await Product.insertMany(products);
+    console.log(newProducts); //debug
+    res.status(201).json(newProducts);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
@@ -91,24 +105,23 @@ app.delete("/products/:id", async (req, res) => {
 app.post("/orders", async (req, res) => {
   try {
     const { products, customerName, customerEmail } = req.body;
-
-    const productIds = products.map((productId) =>
-      mongoose.Types.ObjectId(productId)
+    const productIds = products.map(
+      (productId) => new Types.ObjectId(productId)
     );
     const productsData = await Product.find({ _id: { $in: productIds } });
     const totalPrice = productsData.reduce(
       (total, product) => total + product.price,
       0
     );
-
     const newOrder = new Order({
-      products,
+      products: productIds,
       totalPrice,
       customerName,
       customerEmail,
     });
-    await newOrder.save();
 
+    console.log(newOrder); //debug
+    await newOrder.save();
     res.status(201).json(newOrder);
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -116,10 +129,10 @@ app.post("/orders", async (req, res) => {
 });
 
 // order details
-app.get("/products", async (req, res) => {
+app.get("/orders", async (req, res) => {
   try {
-    const products = await Product.find().populate("orders");
-    res.json(products);
+    const orders = await Order.find();
+    res.json(orders);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
